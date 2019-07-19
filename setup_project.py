a1b489d15f53edbc85f90d0785ac6ed5a4de04bc
idanbrus <brusli1@gmail.com>
2019-07-19 16:48:40 +0300

    restarted the peoject

--- setup_project.py
+++ setup_project.py
@@ -1,16 +1,17 @@
-# 1. Use this script to download the hebrew elmo
-# 2. after downloading, unzip the file
+# 1. Use this script to download the hebrew elmo and the hebrew tree bank
+# 2. after downloading, unzip the file hebrew.zip file
 # 3. change the config.json file, with the path of the configuration file ({rel_path}/ElmoOnMD/ELMoForManyLangs/configs/cnn_50_100_512_4096_sample.json)
 # 4. run the setup file to install the package
 
 import urllib.request
 from itertools import product
+import subprocess
 
 from tqdm import tqdm
 
 if __name__ == '__main__':
     # Download Hebrew ELMo
-    print("Begin Hebrew ELMo download...")
+    print("Beginning Hebrew ELMo download...")
     url = 'http://vectors.nlpl.eu/repository/11/154.zip'
     urllib.request.urlretrieve(url, 'ELMoForManyLangs/hebrew.zip')
     print("Finished downloading")
@@ -20,8 +21,11 @@ if __name__ == '__main__':
     root_url = 'https://raw.github.com/OnlpLab/HebrewResources/master/HebrewTreebank/hebtb/'
     for set, data in tqdm(
             list(product(['train', 'dev', 'test'], ['-gold.conll', '-gold.lattices', '.lattices', '.tokens'])),
-            desc='Downloading..'):
+            desc='Downloading..', unit='file'):
         file = set + '_hebtb' + data
         url = root_url + file
         urllib.request.urlretrieve(url, f'data/hebrew_tree_bank/{file}')
     print("Finished downloading")
+
+    # install the ELMO package
+    subprocess.run("python ELMoForManyLangs/setup.py install")
