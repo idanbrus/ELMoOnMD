5d836bc413efd79a29d45a848c2cb9701b6d58db
idanbrus <brusli1@gmail.com>
2019-07-27 10:31:40 +0300

    first working training

--- elmo_on_md/data_loaders/tree_bank_loader.py
+++ elmo_on_md/data_loaders/tree_bank_loader.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import torch
 
 from elmo_on_md.data_loaders.loader import Loader
 
@@ -22,16 +23,18 @@ class Token_loader(Loader):
         with open(path, 'r', encoding='utf-8') as file:
             content = file.read()
             splat = content.split('\n\n')
-            corpus = [[word.strip() for word in sentence.split('\n')] for sentence in splat]
+            corpus = [[word.strip() for word in sentence.split('\n')] for sentence in splat if sentence.strip()]
 
             return corpus
+
 
 class Morphemes_loader(Loader):
     def __init__(self):
         self.pos_mapping = dict()
         self.max_pos_id = 0
-        self.max_sentence_length = 80 # self measured at the moment
-        self.max_morpheme_count = 49 # self measured at the moment
+        self.max_sentence_length = 80  # self measured at the moment
+        self.max_morpheme_count = 49  # self measured at the moment
+
     def load_data(self) -> dict:
         """
         loads all morphemes to a vector-like structure
@@ -46,7 +49,7 @@ class Morphemes_loader(Loader):
         corpus_dict = {'train': corpus[0], 'dev': corpus[1], 'test': corpus[2]}
         return corpus_dict
 
-    def _map_pos(self,pos):
+    def _map_pos(self, pos):
         if pos not in self.pos_mapping:
             self.pos_mapping[pos] = self.max_pos_id
             self.max_pos_id += 1
@@ -54,36 +57,35 @@ class Morphemes_loader(Loader):
 
     def _get_pos_and_token_id(self, morpheme_data):
         values = morpheme_data.split('\t')
-        return values[-4],int(values[-1])
+        return values[-4], int(values[-1])
 
-    def _set_to_vec(self,set):
+    def _set_to_vec(self, set):
         ans = np.zeros(self.max_morpheme_count)
-        ans[list(set)]=1
+        ans[list(set)] = 1
         return ans
 
-    def _get_sentence_morpheme_map(self,sentence):
+    def _get_sentence_morpheme_map(self, sentence):
         morpheme_data = sentence.split('\n')
-        pairs = [self._get_pos_and_token_id(morpheme_datum.strip()) for morpheme_datum in morpheme_data if morpheme_datum.strip()]
+        pairs = [self._get_pos_and_token_id(morpheme_datum.strip()) for morpheme_datum in morpheme_data if
+                 morpheme_datum.strip()]
         temp = []
         for pair in pairs:
-            if len(temp)>0 and temp[-1][1]==pair[1]:
+            if len(temp) > 0 and temp[-1][1] == pair[1]:
                 temp[-1][0].append(pair[0].strip())
             else:
-                temp.append(([pair[0].strip()],pair[1]))
-        return [(set([self._map_pos(p) for p in vals])) for (vals,pos) in temp]
+                temp.append(([pair[0].strip()], pair[1]))
+        return [(set([self._map_pos(p) for p in vals])) for (vals, pos) in temp]
 
-    def _get_sentence_vector(self,sentence):
+    def _get_sentence_vector(self, sentence):
         mapped = self._get_sentence_morpheme_map(sentence)
-        answer = np.zeros((self.max_sentence_length,self.max_morpheme_count))
+        answer = np.zeros((self.max_sentence_length, self.max_morpheme_count))
         arr = np.array([self._set_to_vec(s) for s in mapped])
-        answer[:arr.shape[0],:arr.shape[1]]=arr
+        answer[:arr.shape[0], :arr.shape[1]] = arr
         return answer
-
-
 
     def _read_morphemes(self, path):
         with open(path, 'r', encoding='utf-8') as file:
             content = file.read()
             splat = content.split('\n\n')
             tensors = [self._get_sentence_vector(sentence.strip()) for sentence in splat if sentence.strip()]
-            return tensors
+            return torch.FloatTensor(tensors)
--- elmo_on_md/model/train.py
+++ elmo_on_md/model/train.py
@@ -1,57 +1,54 @@
+import pickle
 from typing import List
 
 import torch
 from ELMoForManyLangs.elmoformanylangs.elmo import read_list, create_batches
-from torch.nn import L1Loss
 from torch.optim import Adam
 from torch import nn
-from elmo_on_md.data_loaders.tree_bank_loader import Token_loader
+from elmo_on_md.data_loaders.tree_bank_loader import Token_loader, Morphemes_loader
 from elmo_on_md.model.pretrained_models.many_lngs_elmo import get_pretrained_elmo
 
 
 def train():
-    # create the elmo model
+    # create the pretrained elmo model
     embedder = get_pretrained_elmo()
     model = embedder.model
 
     # create training parameters
-    n_epochs = 10
-    total_pos_num = 30
-    max_sentence_length = 50
+    n_epochs = 2
+    total_pos_num = Morphemes_loader().max_morpheme_count
+    max_sentence_length = Morphemes_loader().max_sentence_length
 
     # create input data
     tokens = Token_loader().load_data()['test']
-    test_w, test_c, test_lens, test_masks, test_text, recover_ind = transform_input(tokens, embedder)
+    train_w, train_c, train_lens, train_masks, train_text, recover_ind = transform_input(tokens, embedder)
 
     # create MD data
-    # TODO create MA data
-    md_labels = torch.zeros([len(tokens), max_sentence_length, total_pos_num])
-    md_labels = split_data(md_labels, recover_ind, embedder.batch_size)
+    md_labels = Morphemes_loader().load_data()['test']
+    md_labels = split_data(md_labels, recover_ind, train_lens)
 
     # create the MD module
-    md_model = nn.Sequential(nn.Linear(1024, total_pos_num) ,nn.Sigmoid()) # TODO create architectures
+    md_model = nn.Sequential(nn.Linear(1024, total_pos_num), nn.Sigmoid())  # TODO decide architectures
     full_model = nn.Sequential(model, md_model)
-    criterion = nn.BCELoss(reduction='sum')  # Binary cross entropy
+    criterion = nn.BCELoss()  # Binary cross entropy
     optimizer = Adam(full_model.parameters(), lr=0.01)
 
     for i in range(n_epochs):
         # mini batches
-        for w, c, lens, masks, texts, labels in zip(test_w, test_c, test_lens, test_masks, test_text, md_labels):
+        for w, c, lens, masks, texts, labels in zip(train_w, train_c, train_lens, train_masks, train_text, md_labels):
             optimizer.zero_grad()
 
             # forward + pad with zeros
             output = model.forward(w, c, masks).mean(dim=0)
             output = md_model(output)
             target = torch.zeros((output.shape[0], max_sentence_length, total_pos_num))
-            target[:,:output.shape[1], :] = output
+            target[:, :output.shape[1], :] = output
 
             loss = criterion(target, labels)
             loss.backward()
             optimizer.step()
 
     return embedder
-
-    # embedding = embedder.sents2elmo(tokens)
 
 
 # code taken from ELMO for Many LAN
@@ -65,12 +62,15 @@ def transform_input(tokens, embedder):
     return test_w, test_c, test_lens, test_masks, test_text, recover_ind
 
 
-def split_data(ma_data: torch.tensor, recover_ind: List[int], batch_size: int):
+def split_data(ma_data: torch.tensor, recover_ind: List[int], batch_lens: int):
+    batch_sizes = [len(batch_lens[i]) for i in range(len(batch_lens))]
     indices = torch.tensor(recover_ind)
-    indices = torch.split(indices, batch_size)
+    indices = torch.split(indices, batch_sizes)
     splited_data = [ma_data[batch_indices] for batch_indices in indices]
     return splited_data
 
 
 if __name__ == '__main__':
-    train()
+    new_embedder = train()
+    with open('trained_models/new_model.pkl', 'wb') as file:
+        pickle.dump(new_embedder, file)
