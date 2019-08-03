a77843e7cdf34af3d0fcc970dd5750cb19a3f78b
ShaharBental <shahar.bental@gmail.com>
2019-08-03 14:37:45 +0300

    fixes to model

--- elmo_on_md/evaluation/sentiment_analysis.py
+++ elmo_on_md/evaluation/sentiment_analysis.py
@@ -6,39 +6,37 @@ from torch.optim import Adam
 import numpy as np
 
 
-class RNN(nn.Module):
+class MyRNN(nn.Module):
     def __init__(self, embedding_dim=1024,
                  hidden_dim=256,
                  n_tags=1):
         super().__init__()
         self.hidden_dim = hidden_dim
-        self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=False)
+        # self.lstm = nn.LSTM(input_size=embedding_dim, hidden_size=hidden_dim, bidirectional=False)
+        self.rnn = nn.RNN(embedding_dim, hidden_dim)
         self.hidden2label = nn.Linear(hidden_dim, n_tags)
         self.softmax = nn.Softmax()
 
     def forward(self, input):
-        # hidden = (0,0)
-        # for i in input:
-        hidden = self.initHidden()
-        for x in input:
-            output, (hidden, cell) = self.lstm(input)
+        output, hidden = self.rnn(input)
+        # print(output.shape,hidden.shape,input.shape)
         sigmoid = nn.Sigmoid()
-        output = sigmoid(self.hidden2label(hidden.squeeze())).squeeze()
+        output = self.hidden2label(hidden.squeeze()).squeeze()
         output = self.softmax(output)
         return output
 
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_dim)
+    def initHidden(self, input_size):
+        return torch.zeros((1, input_size, self.hidden_dim))
 
 
 class SentimentAnalysis():
     def __init__(self, elmo: Embedder, n_tags=3):
         self.elmo = elmo
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = RNN(n_tags=n_tags)
+        self.model = MyRNN(n_tags=n_tags)
         weights = torch.ones(n_tags)
         weights = torch.cat([weights, torch.ones(1)]).to(self.device)
-        self.criterion = nn.MultiLabelSoftMarginLoss()  # Binary cross entropy
+        self.criterion = nn.CrossEntropyLoss()  # Binary cross entropy
         self.optimizer = Adam(self.model.parameters(), lr=1e-3)
         self.max_sentence_length = 100
 
@@ -82,12 +80,7 @@ class SentimentAnalysis():
         return input
 
     def _create_labels(self, train_set):
-        # return torch.from_numpy(np.array(train_set['labels']).astype('long'))
-        t = torch.LongTensor(train_set['labels'])
-        t = t.view(t.shape[0], 1)
-        tt = torch.Tensor(t.shape[0], 3)
-        tt.zero_()
-        return tt.scatter_(1, t, 1)
+        return torch.from_numpy(np.array(train_set['labels']).astype('long')).long()
 
     def _chunker_list(self, train_set, n):
         s = train_set['sentences']
